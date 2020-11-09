4481305196e6039fad30fa48da1fd00e4afee5ac
baozounan <874974467@qq.com>
2020-11-09 22:43:44 +0800

    修改发送udp信息的方法

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from udpMethods.socket_udp_receive import *
+from udpMethods.socket_udp_send import *
 
 
 
@@ -7,4 +8,6 @@ from udpMethods.socket_udp_receive import *
 
 if __name__ == '__main__':
     udp_receive_message("127.0.0.1",7080)
+    #udp_send_message("")
+
 
