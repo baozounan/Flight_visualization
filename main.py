725eb2f606dbbba7a272d3887ebb16800f347042
odin-gungnir <18406550406@163.com>
2020-11-10 23:05:41 +0800

    2020.11.1  output.xml文件编写，添加了经度、纬度、气压高度、外界大气温度 未编写高度

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from udpMethods.socket_udp_send import *
 
 
 if __name__ == '__main__':
-    udp_receive_message("127.0.0.1",7080)
+    udp_receive_message("127.0.0.1", 7080)
     #udp_send_message("")
 
 
