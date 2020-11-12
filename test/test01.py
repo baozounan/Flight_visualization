1216c7e691be71ecb94d2c60346089b07feb6062
baozounan <874974467@qq.com>
2020-11-12 20:18:25 +0800

    将飞机飞行的数据转换成了字典，可以通过键来取值，增加了发动机转速数据，修改了xml文件格式

--- test/test01.py
+++ test/test01.py
@@ -1,8 +1,8 @@
 from utils.convert import *
+import json
 def main():
-    str = "AGL:8745"
-    dict = str2dict(str)
-    print(type(dict))
+    str='{"Pos":2962.7,"M":0.1,"GS":"64kts","G":1.2,"AGL":2967}'
+    dict = json.loads(str)
     print(dict)
 if __name__ == '__main__':
     main()
--- udpMethods/socket_udp_receive.py
+++ udpMethods/socket_udp_receive.py
@@ -1,5 +1,6 @@
 # encoding:utf-8
 import socket
+import json
 def udp_receive_message(ip_addr,port):
     """使用udp连接方式循环接收数据，输入IP地址(字符串类型)和端口（数值类型）"""
     ip_addr_str = str(ip_addr)  # 转换成字符串类型
@@ -8,16 +9,23 @@ def udp_receive_message(ip_addr,port):
     # 2.绑定本地信息
     addr = (ip_addr_str,port)
     udp_socket.bind(addr)
+    # 3.创建存储发动机转速数据的结构
+    engine_rpm_data=[]
     # 3接受数据
     while True:
         rec_data = udp_socket.recvfrom(1024)
         rec_msg = rec_data[0].decode()
-        send_addr = rec_data[1]
+        str_recmsg = "{"+rec_msg[:-1]+"}"
+        dict_recmsg = json.loads(str_recmsg)
+        engine_rpm_data.append(dict_recmsg.get("engine_rpm"))
+        #send_addr = rec_data[1] 主机地址信息
         # 打印收到的数据
         # print(rec_data)
-        print(rec_msg)
+        print(engine_rpm_data)
         #print(rec_data)  #数据来源
-        print(type(rec_msg))
+        #print(type(data))
         # print("%s:%s" % (str(send_addr), rec_msg.decode("gb18030")))
     # 关闭套接字
     udp_socket.close()
+    return data
+
