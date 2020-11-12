5b74ea6d89372c22edcbacd5e381f5d20e1dc512
odin-gungnir <18406550406@163.com>
2020-11-12 22:49:25 +0800

    2020.11.1  output.xml文件编写，添加了地面速度，升降速度，航向角，左油箱油量，右油箱油量，燃油流量

--- udpMethods/socket_udp_receive.py
+++ udpMethods/socket_udp_receive.py
@@ -9,19 +9,21 @@ def udp_receive_message(ip_addr,port):
     # 2.绑定本地信息
     addr = (ip_addr_str,port)
     udp_socket.bind(addr)
-    # 3.创建存储发动机转速数据的结构
-    engine_rpm_data=[]
-    # 3接受数据
+    # 3.创建经度和纬度数据的结构
+    latitude_deg_data=[]
+    longitude_deg_data=[]
+    # 3.接受数据
     while True:
         rec_data = udp_socket.recvfrom(1024)
         rec_msg = rec_data[0].decode()
         str_recmsg = "{"+rec_msg[:-1]+"}"
         dict_recmsg = json.loads(str_recmsg)
-        engine_rpm_data.append(dict_recmsg.get("engine_rpm"))
+        latitude_deg_data.append(dict_recmsg.get("latitude-deg"))
+        longitude_deg_data.append(dict_recmsg.get("longititude-deg"))
         #send_addr = rec_data[1] 主机地址信息
         # 打印收到的数据
-        # print(rec_data)
-        print(engine_rpm_data)
+        print(dict_recmsg)
+        #print(engine_rpm_data)
         #print(rec_data)  #数据来源
         #print(type(data))
         # print("%s:%s" % (str(send_addr), rec_msg.decode("gb18030")))
